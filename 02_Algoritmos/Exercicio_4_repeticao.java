86b9a87e017d882a9e60913d1d3ba1c258801072
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-11-05 00:11:47 -0300

    exercicio 4

--- 02_Algoritmos/Exercicio_4_repeticao.java
+++ 02_Algoritmos/Exercicio_4_repeticao.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
+
 public class Exercicio_4_repeticao {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        int soma = 0;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Digite um número: ");
+            int numero = scanner.nextInt();
+            soma += numero;
+            System.out.println("Soma parcial: " + soma);
+        }
+
+        System.out.println("Soma final: " + soma);
+        scanner.close();
     }
 }
