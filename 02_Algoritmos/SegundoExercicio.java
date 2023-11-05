5254addc1f84ff8e016f34735ba5054796314684
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-11-04 22:53:38 -0300

    Segundo exercicio

--- 02_Algoritmos/SegundoExercicio.java
+++ 02_Algoritmos/SegundoExercicio.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class SegundoExercicio {
     public static void main(String[] args) {
-        
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Digite o primeiro número: ");
+            int n1 = scanner.nextInt();
+            System.out.print("Digite o segundo número: ");
+            int n2 = scanner.nextInt();
+            
+            System.out.println("n1 + n2 = " + (n1 + n2));
+        }
     }
 }
