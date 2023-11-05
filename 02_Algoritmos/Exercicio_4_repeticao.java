7813ee45c03e75550f9d700b57bc0e8ad5f30777
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-11-05 00:20:01 -0300

    Otimização do código

--- 02_Algoritmos/Exercicio_4_repeticao.java
+++ 02_Algoritmos/Exercicio_4_repeticao.java
@@ -3,17 +3,23 @@ import java.util.Scanner;
 
 public class Exercicio_4_repeticao {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int soma = 0;
+        try (Scanner scanner = new Scanner(System.in)) {
+            int soma = 0;
+            int i;
+            int numero = 0;
 
-        for (int i = 0; i < 3; i++) {
-            System.out.print("Digite um número: ");
-            int numero = scanner.nextInt();
-            soma += numero;
-            System.out.println("Soma parcial: " + soma);
+            for (i = 0; i < 3; i++) {
+                System.out.print("Digite um número: ");
+                numero = scanner.nextInt();
+                soma += numero;
+                System.out.println("iteração: " + i);
+                System.out.println("Soma parcial: " + soma);
+                System.out.println("num: " + numero);
+            }
+            System.out.println("iteração: " + i);
+            System.out.println("Soma final: " + soma);
+            System.out.println("num: " + numero);
+            scanner.close();
         }
-
-        System.out.println("Soma final: " + soma);
-        scanner.close();
     }
 }
