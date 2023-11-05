fe62066314af0a6fe3d1286ee54078e63038e895
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-11-05 00:51:39 -0300

    Solução usando listas

---
+++ 02_Algoritmos/Exercicio_5_listas_arrayAlt.java
@@ -0,0 +1,27 @@
+// Solução usando array/listas
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Exercicio_5_listas_arrayAlt {
+    public static void main(String[] args) {
+        List<Integer> numerosDivisiveis = new ArrayList<>();
+        int numero = 1;  // Começamos a partir do primeiro número e verificamos se é divisível.
+
+        while (numerosDivisiveis.size() < 20) {
+            if (numero % 2 == 0 || numero % 5 == 0) {
+                numerosDivisiveis.add(numero);
+            }
+            numero++;
+        }
+
+        System.out.print("Os primeiros 20 números divisíveis por 2 ou por 5 são: [");
+        for (int i = 0; i < numerosDivisiveis.size(); i++) {
+            System.out.print(numerosDivisiveis.get(i));
+            if (i < numerosDivisiveis.size() - 1) {
+                System.out.print(", "); // Adiciona vírgula apenas se não for o último número
+            }
+        }
+        System.out.println("]");
+    }
+}
