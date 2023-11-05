97cb1bab6fcc9481e23137fcd3c325649f4a4071
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-11-04 22:37:43 -0300

    corrigido leak de scanner

--- PrimeiroExercicio.java
+++ PrimeiroExercicio.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 public class PrimeiroExercicio {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.print("Digite seu nome: ");
-        String nome = scanner.nextLine();
-        
-        System.out.println("Bom dia, " + nome);
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Digite seu nome: ");
+            String nome = scanner.nextLine();
+            
+            System.out.println("Bom dia, " + nome);
+        }
     }
 }
