d6bf7dfedb96cb9c9afceb610389e81f8998a6e3
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-11-07 19:32:43 -0300

    Aula de Variaveis

--- Introducao_ao_Java/Variaveis.java
+++ Introducao_ao_Java/Variaveis.java
@@ -1,5 +1,25 @@
 public class Variaveis {
     public static void main(String[] args) {
-        
+        // write your code here
+
+        String nome;
+        nome = "Let's Code";
+        nome = "Marcos";
+        nome = "Brasil";
+        System.out.println("Olá, " + nome);
+        System.out.println("--------------------------------");
+        int a;
+        int b = 2;
+        a = 3;
+        int soma = a + b;
+        int subtracao = a -b;
+        int multiplicacao = a * b;
+        // fazendo cast
+        float divisao = (float) a / b;
+
+        System.out.println(soma);
+        System.out.println(subtracao);
+        System.out.println(multiplicacao);
+        System.out.println(divisao);
     }
 }
