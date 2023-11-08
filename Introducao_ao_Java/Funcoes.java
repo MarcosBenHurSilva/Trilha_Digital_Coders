3a4bd096400562371a4fbbbe8525e39ee6548759
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-11-07 22:24:14 -0300

    Primeira parte da aula

---
+++ Introducao_ao_Java/Funcoes.java
@@ -0,0 +1,11 @@
+public class Funcoes {
+    public static void main(String[] args) {
+        String nomeOriginal = "Marcos";
+        saudacao(nomeOriginal);
+    }
+
+    // função ou método
+    public static void saudacao(String nomeParametro){
+        System.out.println("Hello, " + nomeParametro);
+    }
+}
