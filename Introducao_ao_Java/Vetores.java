b1b4948b3a8d758ba1bb795966a3a5cedf27eb1d
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-11-07 22:14:31 -0300

    Parte final da aula de Vetores

--- Introducao_ao_Java/Vetores.java
+++ Introducao_ao_Java/Vetores.java
@@ -1,22 +1,21 @@
-import java.util.Arrays;
-
 public class Vetores {
     public static void main(String[] args) {
-        // ctrl + c -> para comentar
-        // String[] letras = new String[5];
-        // // [0] [1] [2] [3] [4]
-        // letras[0] = "A";
-        // letras[1] = "B";
-        // letras[2] = "C";
-        // letras[3] = "J";
-        // letras[4] = "X";
-        // for (int i=0; i < letras.length; i++){
-        //     System.out.println(letras[i]);
-        // }
-        String[] letras = {"A", "B", "C", "J", "X", "EXTRA"};
-        for (int i=0; i < letras.length; i++){
-            System.out.println(letras[i]);
+        int[] numeros = {9, 10, 12, 25, 2};
+        int maior = numeros[0];
+        int menor = numeros[0];
+        int media = 0;
+
+        for (int i = 0; i < numeros.length; i++){
+            if (numeros[i] > maior){
+                maior = numeros[i];
+            }
+            if (numeros[i] < menor) {
+                menor = numeros[i];
+            }
+            media += numeros[i];
         }
-        System.out.println(Arrays.toString(letras));
+        System.out.println("Maior: " + maior);
+        System.out.println("Menor: " + menor);
+        System.out.println("Media: " + media/numeros.length);
     }
 }
