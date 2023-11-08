977ae9ef11a56c3dac97b6dd3a5446db29aec7cf
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-11-07 22:06:52 -0300

    Terceira parte da aula

--- Introducao_ao_Java/Vetores.java
+++ Introducao_ao_Java/Vetores.java
@@ -1,14 +1,22 @@
+import java.util.Arrays;
+
 public class Vetores {
     public static void main(String[] args) {
-        String[] letras = new String[5];
-        // [0] [1] [2] [3] [4]
-        letras[0] = "A";
-        letras[1] = "B";
-        letras[2] = "C";
-        letras[3] = "J";
-        letras[4] = "X";
+        // ctrl + c -> para comentar
+        // String[] letras = new String[5];
+        // // [0] [1] [2] [3] [4]
+        // letras[0] = "A";
+        // letras[1] = "B";
+        // letras[2] = "C";
+        // letras[3] = "J";
+        // letras[4] = "X";
+        // for (int i=0; i < letras.length; i++){
+        //     System.out.println(letras[i]);
+        // }
+        String[] letras = {"A", "B", "C", "J", "X", "EXTRA"};
         for (int i=0; i < letras.length; i++){
             System.out.println(letras[i]);
         }
+        System.out.println(Arrays.toString(letras));
     }
 }
