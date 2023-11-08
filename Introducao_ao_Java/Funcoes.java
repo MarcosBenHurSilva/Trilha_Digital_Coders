45494828e9392ee0673be87cadb8093fd49a66bc
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-11-07 22:30:10 -0300

    Parte Final da aula de Funções

--- Introducao_ao_Java/Funcoes.java
+++ Introducao_ao_Java/Funcoes.java
@@ -1,11 +1,9 @@
 public class Funcoes {
     public static void main(String[] args) {
-        String nomeOriginal = "Marcos";
-        saudacao(nomeOriginal);
+        int resultado = soma(2, 3);
+        System.out.println(resultado);
     }
-
-    // função ou método
-    public static void saudacao(String nomeParametro){
-        System.out.println("Hello, " + nomeParametro);
+    public static int soma(int a, int b){
+        return a + b;
     }
 }
