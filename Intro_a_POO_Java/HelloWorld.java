a5fb95cebc1eea99db6865b97ee3ad84c7f1f427
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-11-08 21:27:28 -0300

    Aula hello world

---
+++ Intro_a_POO_Java/HelloWorld.java
@@ -0,0 +1,24 @@
+package Intro_a_POO_Java;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        // byte -128 a 127
+        byte variavelByte = -127;
+        System.out.println(variavelByte);
+        // short -32768 a 32767
+        short variavelShort = 32767;
+        System.out.println(variavelShort);
+        // char de 0 a 65535
+        char variavelChar = 5;
+        System.out.println(variavelChar);
+        // int -2 Bilhões a 2 Bilhões
+        int variavelInt = 1000;
+        System.out.println(variavelInt);
+        // long -9 trilhões a 9 trilhões
+        long variavelLong = 2147483;
+        System.out.println(variavelLong);
+        
+    }
+}
